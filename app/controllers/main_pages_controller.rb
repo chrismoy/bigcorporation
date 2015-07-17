f66ea889398a5d7de9ad6b22fb4aa47c36e0467e
Chris Moy <moyboy530@gmail.com>
2015-07-16 23:19:40 -0500

    Add mainpages controller and views

---
+++ app/controllers/main_pages_controller.rb
@@ -0,0 +1,16 @@
+class MainPagesController < ApplicationController
+  def home
+  end
+
+  def stuff
+  end
+
+  def team
+  end
+
+  def about
+  end
+
+  def contact
+  end
+end
---
+++ test/controllers/main_pages_controller_test.rb
@@ -0,0 +1,29 @@
+require 'test_helper'
+
+class MainPagesControllerTest < ActionController::TestCase
+  test "should get home" do
+    get :home
+    assert_response :success
+  end
+
+  test "should get stuff" do
+    get :stuff
+    assert_response :success
+  end
+
+  test "should get team" do
+    get :team
+    assert_response :success
+  end
+
+  test "should get about" do
+    get :about
+    assert_response :success
+  end
+
+  test "should get contact" do
+    get :contact
+    assert_response :success
+  end
+
+end

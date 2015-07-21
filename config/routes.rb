f71eb4f84e0abe07c361862588981503fb1743b2
Chris Moy <moyboy530@gmail.com>
2015-07-20 21:16:01 -0500

    Start setting up routes and layouts

--- config/routes.rb
+++ config/routes.rb
@@ -1,21 +1,9 @@
 Rails.application.routes.draw do
-  get 'main_pages/home'
-
-  get 'main_pages/stuff'
-
-  get 'main_pages/team'
-
-  get 'main_pages/about'
-
-  get 'main_pages/contact'
-
-  get 'home/stuff'
-
-  get 'home/team'
-
-  get 'home/about'
-
-  get 'home/contact'
-
   root    'main_pages#home'
+
+  get     'main_pages/home'
+  get     'main_pages/stuff'
+  get     'main_pages/team'
+  get     'main_pages/about'
+  get     'main_pages/contact'
 end

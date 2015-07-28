42596648d2650d6f4afda5e901f0f91e35ac0ef8
Chris Moy <moyboy530@gmail.com>
2015-07-28 00:23:07 -0500

    Add in staffers home page

--- app/controllers/allstaff_controller.rb
+++ app/controllers/allstaff_controller.rb
@@ -2,6 +2,15 @@ class AllstaffController < ApplicationController
   layout "allstaff"
 
   def home
+    @staffers = [["Select Staffing"],
+    ["Fellows Staffing"],
+    ["Jeane Thorne Staffing"],
+    ["Select Advantage Staffing"],
+    ["Digital Staffing"],
+    ["Andrews Staffing"],
+    ["Behavioral Staffing"],
+    ["Fellows Financial"],
+    ["All-Calls"]]
   end
 
   def select

788957e1141e43f2b65f5aa0a5bb575af49a7a2f
Chris Moy <moyboy530@gmail.com>
2015-07-30 11:26:56 -0500

    Updates

--- app/assets/javascripts/allstaff.js
+++ app/assets/javascripts/allstaff.js
@@ -13,4 +13,17 @@ $(window).resize(resizing);
 function ready() {
   setCompanyHeights();
   initialLogoPositions();
+
+  $('#modal-button').click(function() {
+    obscure();
+    $('#modal').show();
+  });
+  $('#modal-hide').click(function() {
+    obscure();
+    $('#modal').hide();
+  });
+}
+
+function obscure() {
+  $('.content').toggleClass('obscure');
 }

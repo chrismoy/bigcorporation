379ede73562ba5634c0b08f8493620c86a3376e2
Chris Moy <moyboy530@gmail.com>
2015-08-11 13:55:40 -0500

    Change responsive elements

--- config/routes.rb
+++ config/routes.rb
@@ -6,8 +6,4 @@ Rails.application.routes.draw do
   get     'main_pages/team'
   get     'main_pages/about'
   get     'main_pages/contact'
-  get     'allstaff/home'
-  get     'allstaff/select'
-  get     'allstaff/portal'
-  get     'allstaff/capabilities'
 end

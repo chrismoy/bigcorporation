5555bfc2a1e2b5e842b1a98ab2cf1c01121d6de0
Chris Moy <moyboy530@gmail.com>
2016-01-24 01:47:12 -0600

    Create location independent buttons for navbar

--- app/assets/javascripts/app.menu.js
+++ app/assets/javascripts/app.menu.js
@@ -1,9 +1,11 @@
 App.Menu = {
   toggle: function() {
-    $('.menu').fadeToggle( "200", "easeInCubic" );
+    $('.menu').fadeToggle( "200" );
   }
 }
 
 $(document).on( "click", "[data-behavior~=toggle-menu]", function() {
   $(this).toggleClass("btn-hamburger-active");
+  // $('header .btn').toggleClass( "active" );
+  App.Menu.toggle();
 });

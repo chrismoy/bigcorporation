96dad505f2c8a38aaa6e78faace174ac17149949
Chris Moy <moyboy530@gmail.com>
2016-03-09 23:00:17 -0600

    work on framework

--- db/schema.rb
+++ db/schema.rb
@@ -11,16 +11,9 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150831033740) do
+ActiveRecord::Schema.define(version: 0) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
-
-  create_table "pages", force: :cascade do |t|
-    t.string   "name"
-    t.string   "background"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
 
 end
